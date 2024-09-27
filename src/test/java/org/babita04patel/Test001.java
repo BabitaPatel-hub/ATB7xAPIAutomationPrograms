26c291c2b71d58b1e243dfb7b52b05dd2588775c
BabitaPatel-hub <babita04patel@gmail.com>
2024-09-28 00:23:41 +0530

    Initial commit

---
+++ src/test/java/org/babita04patel/Test001.java
@@ -0,0 +1,11 @@
+package org.babita04patel;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
